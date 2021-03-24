dff7c3bf722855883fddba4f415f261e25eee4ba
Rob Dickinson <robfromboulder@gmail.com>
2021-03-24 17:20:55 -0600

    Updates for 2.2 schema

--- src/main/java/io/resurface/trino/connector/ResurfaceRecordCursor.java
+++ src/main/java/io/resurface/trino/connector/ResurfaceRecordCursor.java
@@ -73,11 +73,11 @@ public class ResurfaceRecordCursor implements RecordCursor {
         switch (column_ordinal_positions[field]) {
             case 7:
                 return message.interval_millis.value();
-            case 19:
-                return message.response_time_millis.value();
             case 21:
-                return message.size_request_bytes.value();
+                return message.response_time_millis.value();
             case 22:
+                return message.size_request_bytes.value();
+            case 23:
                 return message.size_response_bytes.value();
             default:
                 throw new IllegalArgumentException("Cannot get as long: " + column_names[field]);
@@ -106,11 +106,11 @@ public class ResurfaceRecordCursor implements RecordCursor {
             case 3:
                 return getSliceFromField(message.agent_name);
             case 4:
-                return getSliceFromField(message.host);
+                return getSliceFromField(message.graphql_operation);
             case 5:
-                return getSliceFromField(message.interval_category);
+                return getSliceFromField(message.graphql_operation_name);
             case 6:
-                return getSliceFromField(message.interval_clique);
+                return getSliceFromField(message.host);
             case 8:
                 return getSliceFromField(message.request_body);
             case 9:
@@ -118,23 +118,25 @@ public class ResurfaceRecordCursor implements RecordCursor {
             case 10:
                 return getSliceFromField(message.request_headers);
             case 11:
-                return getSliceFromField(message.request_method);
+                return getSliceFromField(message.request_json_type);
             case 12:
-                return getSliceFromField(message.request_params);
+                return getSliceFromField(message.request_method);
             case 13:
-                return getSliceFromField(message.request_url);
+                return getSliceFromField(message.request_params);
             case 14:
-                return getSliceFromField(message.request_user_agent);
+                return getSliceFromField(message.request_url);
             case 15:
-                return getSliceFromField(message.response_body);
+                return getSliceFromField(message.request_user_agent);
             case 16:
-                return getSliceFromField(message.response_code);
+                return getSliceFromField(message.response_body);
             case 17:
-                return getSliceFromField(message.response_content_type);
+                return getSliceFromField(message.response_code);
             case 18:
+                return getSliceFromField(message.response_content_type);
+            case 19:
                 return getSliceFromField(message.response_headers);
             case 20:
-                return getSliceFromField(message.size_category);
+                return getSliceFromField(message.response_json_type);
             default:
                 throw new IllegalArgumentException("Cannot get as string: " + column_names[field]);
         }
@@ -162,11 +164,11 @@ public class ResurfaceRecordCursor implements RecordCursor {
             case 3:
                 return message.agent_name.length() == 0;
             case 4:
-                return message.host.length() == 0;
+                return message.graphql_operation.length() == 0;
             case 5:
-                return message.interval_category.length() == 0;
+                return message.graphql_operation_name.length() == 0;
             case 6:
-                return message.interval_clique.length() == 0;
+                return message.host.length() == 0;
             case 7:
                 return message.interval_millis.value() == 0;
             case 8:
@@ -176,28 +178,30 @@ public class ResurfaceRecordCursor implements RecordCursor {
             case 10:
                 return message.request_headers.length() == 0;
             case 11:
-                return message.request_method.length() == 0;
+                return message.request_json_type.length() == 0;
             case 12:
-                return message.request_params.length() == 0;
+                return message.request_method.length() == 0;
             case 13:
-                return message.request_url.length() == 0;
+                return message.request_params.length() == 0;
             case 14:
-                return message.request_user_agent.length() == 0;
+                return message.request_url.length() == 0;
             case 15:
-                return message.response_body.length() == 0;
+                return message.request_user_agent.length() == 0;
             case 16:
-                return message.response_code.length() == 0;
+                return message.response_body.length() == 0;
             case 17:
-                return message.response_content_type.length() == 0;
+                return message.response_code.length() == 0;
             case 18:
-                return message.response_headers.length() == 0;
+                return message.response_content_type.length() == 0;
             case 19:
-                return message.response_time_millis.value() == 0;
+                return message.response_headers.length() == 0;
             case 20:
-                return message.size_category.length() == 0;
+                return message.response_json_type.length() == 0;
             case 21:
-                return message.size_request_bytes.value() == 0;
+                return message.response_time_millis.value() == 0;
             case 22:
+                return message.size_request_bytes.value() == 0;
+            case 23:
                 return message.size_response_bytes.value() == 0;
             default:
                 throw new IllegalArgumentException("Invalid field index: " + field);
--- src/main/java/io/resurface/trino/connector/ResurfaceTables.java
+++ src/main/java/io/resurface/trino/connector/ResurfaceTables.java
@@ -58,29 +58,30 @@ public class ResurfaceTables {
     public static final class MessageTable {
 
         public static final List<ColumnMetadata> COLUMNS = ImmutableList.of(
-                new ColumnMetadata("id", createUnboundedVarcharType()),                     // 0
-                new ColumnMetadata("agent_category", createUnboundedVarcharType()),         // 1
-                new ColumnMetadata("agent_device", createUnboundedVarcharType()),           // 2
-                new ColumnMetadata("agent_name", createUnboundedVarcharType()),             // 3
-                new ColumnMetadata("host", createUnboundedVarcharType()),                   // 4
-                new ColumnMetadata("interval_category", createUnboundedVarcharType()),      // 5
-                new ColumnMetadata("interval_clique", createUnboundedVarcharType()),        // 6
-                new ColumnMetadata("interval_millis", BIGINT),                              // 7
-                new ColumnMetadata("request_body", createUnboundedVarcharType()),           // 8
-                new ColumnMetadata("request_content_type", createUnboundedVarcharType()),   // 9
-                new ColumnMetadata("request_headers", createUnboundedVarcharType()),        // 10
-                new ColumnMetadata("request_method", createUnboundedVarcharType()),         // 11
-                new ColumnMetadata("request_params", createUnboundedVarcharType()),         // 12
-                new ColumnMetadata("request_url", createUnboundedVarcharType()),            // 13
-                new ColumnMetadata("request_user_agent", createUnboundedVarcharType()),     // 14
-                new ColumnMetadata("response_body", createUnboundedVarcharType()),          // 15
-                new ColumnMetadata("response_code", createUnboundedVarcharType()),          // 16
-                new ColumnMetadata("response_content_type", createUnboundedVarcharType()),  // 17
-                new ColumnMetadata("response_headers", createUnboundedVarcharType()),       // 18
-                new ColumnMetadata("response_time_millis", BIGINT),                         // 19
-                new ColumnMetadata("size_category", createUnboundedVarcharType()),          // 20
-                new ColumnMetadata("size_request_bytes", INTEGER),                          // 21
-                new ColumnMetadata("size_response_bytes", INTEGER)                          // 22
+                new ColumnMetadata("id", createUnboundedVarcharType()),                            // 0
+                new ColumnMetadata("agent_category", createUnboundedVarcharType()),                // 1
+                new ColumnMetadata("agent_device", createUnboundedVarcharType()),                  // 2
+                new ColumnMetadata("agent_name", createUnboundedVarcharType()),                    // 3
+                new ColumnMetadata("graphql_operation", createUnboundedVarcharType()),             // 4
+                new ColumnMetadata("graphql_operation_name", createUnboundedVarcharType()),        // 5
+                new ColumnMetadata("host", createUnboundedVarcharType()),                          // 6
+                new ColumnMetadata("interval_millis", BIGINT),                                     // 7
+                new ColumnMetadata("request_body", createUnboundedVarcharType()),                  // 8
+                new ColumnMetadata("request_content_type", createUnboundedVarcharType()),          // 9
+                new ColumnMetadata("request_headers", createUnboundedVarcharType()),               // 10
+                new ColumnMetadata("request_json_type", createUnboundedVarcharType()),             // 11
+                new ColumnMetadata("request_method", createUnboundedVarcharType()),                // 12
+                new ColumnMetadata("request_params", createUnboundedVarcharType()),                // 13
+                new ColumnMetadata("request_url", createUnboundedVarcharType()),                   // 14
+                new ColumnMetadata("request_user_agent", createUnboundedVarcharType()),            // 15
+                new ColumnMetadata("response_body", createUnboundedVarcharType()),                 // 16
+                new ColumnMetadata("response_code", createUnboundedVarcharType()),                 // 17
+                new ColumnMetadata("response_content_type", createUnboundedVarcharType()),         // 18
+                new ColumnMetadata("response_headers", createUnboundedVarcharType()),              // 19
+                new ColumnMetadata("response_json_type", createUnboundedVarcharType()),            // 20
+                new ColumnMetadata("response_time_millis", BIGINT),                                // 21
+                new ColumnMetadata("size_request_bytes", INTEGER),                                 // 22
+                new ColumnMetadata("size_response_bytes", INTEGER)                                 // 23
         );
 
         public static final String TABLE_NAME = "message";
