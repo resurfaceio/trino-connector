cb6f7966964e3e0c6e540a05cc4a6965e2fb6ddb
Rob Dickinson <robfromboulder@gmail.com>
2021-09-13 15:18:38 -0600

    Skip rebuild on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-mvn clean
-mvn package
 mv target/resurfaceio-trino-connector-$1.zip target/resurfaceio-trino-connector.zip
 cloudsmith push raw resurfacelabs/internal target/resurfaceio-trino-connector.zip --version $1
